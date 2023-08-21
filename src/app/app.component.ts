795579cb3e9c39941c0a0aa8b914952b2cd309a3
Lucas <lucasgoncalves08@live.com>
2023-08-21 10:47:18 -0300

    finishing toggle button styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    './styles/toggle-button.css',
+    './styles/header.css'
+  ]
 })
 export class AppComponent {
   title = 'dictionary-web-app';
